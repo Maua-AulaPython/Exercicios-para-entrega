86b22c2c7a270b47abc535375d23361cf42a0bad
ricardocascino <ricardocascino@hotmail.com>
2015-03-20 07:17:33 -0300

    Ex2_par.py
    O programa retornará a informação se o seu numero é par ou impar.

---
+++ Ex2_par.py
@@ -0,0 +1,8 @@
+print ("Descobrindo se o seu numero é par ou impar")
+x=int (raw_input("digite o seu numero:"))
+r=x%2
+if (r==0):
+    print ("o seu nuemro é par")
+else:
+    print ("o seu numero é impar")
+    
