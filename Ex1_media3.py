0ea808e2e344ba639dd4b723b645204413cc2d74
ricardocascino <ricardocascino@hotmail.com>
2015-03-20 07:20:18 -0300

    Ex1_media3.py
    Este código retornara a media dos 3 numeros inseridos pelo suario

---
+++ Ex1_media3.py
@@ -0,0 +1,7 @@
+print ("vamos fazer medias entre tres valores")
+x=float (raw_input("primeiro numero:"))
+y=float (raw_input("segundo numero:"))
+w=float (raw_input("terceiro numero:"))
+r=((x+y+w)/3)
+print (" a sua media é:")
+print r
