22e28d858127791a57d8fdda2aca3b2fcc345275
ricardocascino <ricardocascino@hotmail.com>
2015-03-20 11:11:43 -0300

    Ex3_medias2.py

---
+++ Ex3_medias2.py
@@ -0,0 +1,15 @@
+valor=0
+b=[]
+res=0
+i=0
+print ("digite quantos numeros vc quiser para fazer a media deles")
+print ("quando desejar parar de inserir numeros, digite um numero negativo")
+while valor>=0:
+    i= i+1
+    valor=float(raw_input("numero %i da media:" %i))
+    if valor>=0:
+        b.append(valor)
+for g in b:
+    res=res+g
+resultado=res/(i-1)
+print resultado
